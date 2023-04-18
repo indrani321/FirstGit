0d46c814880620d59a9874a7ba93f7aca430639e
indrani321 <131009169+indrani321@users.noreply.github.com>
2023-04-18 20:55:17 +0530

    GetElementByClassname task done

--- dom.js
+++ dom.js
@@ -1,5 +1,12 @@
 var title=document.getElementById("main-header");
 title.style.borderBottom='solid 5px black';
 var item=document.getElementById("add");
-item.style.fontStyle='bold';
+item.style.fontWeight='bold';
 item.style.color="green";
+var a=document.getElementsByClassName('list-group-item');
+a[2].style.backgroundColor='green';
+for(var i=0;i<a.length;i++)
+{
+    a[i].style.fontWeight='bold';
+}
+
