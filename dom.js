736a8cfe95da74c43bb7948a080fd79d2c1e38af
indrani321 <131009169+indrani321@users.noreply.github.com>
2023-04-18 20:39:18 +0530

    Add Items Font Colors Change To Green.

---
+++ dom.js
@@ -0,0 +1,5 @@
+var title=document.getElementById("main-header");
+title.style.borderBottom='solid 5px black';
+var item=document.getElementById("add");
+item.style.fontStyle='bold';
+item.style.color="green";
